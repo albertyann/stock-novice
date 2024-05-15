12bbc47166080a1df4aea3227291aa0a67a2a147
zijing <zijing@wshoto.com>
2024-05-15 21:45:44 +0800

    页面可以刷新股票

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,15 +1,36 @@
 <script setup>
-import { onMounted, onUnmounted } from 'vue'
+import { onMounted, onUnmounted, ref } from 'vue'
 import { init, dispose } from 'klinecharts'
 import axios from 'axios'
 
-onMounted(() => {
-  const chart = init('chart')
 
-  axios.get("http://127.0.0.1:5000/api/kline")
+const stocks = ref([])
+const asymbol = ref('')
+const chart = {}
+
+function refresh() {
+  axios.get("http://127.0.0.1:5000/api/fetch/kline/"+asymbol.value)
   .then(response => {
     console.log(response)
-    const data = response.data
+    loadStock(asymbol.value)
+  })
+  .catch(e => {
+    this.errors.push(e)
+  })
+}
+
+function loadStock(symbol) {
+  if (symbol === '') {
+    return
+  }
+
+  console.log(symbol)
+  asymbol.value = symbol
+  axios.get("http://127.0.0.1:5000/api/kline/"+symbol)
+  .then(response => {
+    
+
+    const data = response.data.data
     const chartData = data.map(item => {
       return {
         timestamp: item[0],
@@ -20,10 +41,22 @@ onMounted(() => {
         close:     item[5]
       }
     })
-    chart.applyNewData(chartData)
+    console.log(chartData)
+    chart.chart.applyNewData(chartData)
   })
   .catch(e => {
     this.errors.push(e)
+  })
+}
+
+onMounted(() => {
+  chart.chart = init('chart')
+
+  axios.get("http://127.0.0.1:5000/api/stock")
+  .then(response => {
+    console.log(response)
+    stocks.value = response.data.data
+    console.log(stocks)
   })
 })
 
@@ -33,5 +66,22 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <div id="chart" style="width:600px;height:300px" />
+  <div>
+    <h3>今日关注</h3>
+    <div>
+      <label v-for="stock in stocks" :key="stock.symbol">
+        <button @click="loadStock(stock.symbol)">{{ stock.name }}</button>
+      </label>
+    </div>
+  </div>
+  <div>
+    <input type="text" v-model="asymbol.symbol" />
+    <button @click="loadStock">Load</button>
+  </div>
+  <div>
+    <label>
+      <button @click="refresh">{{ asymbol }} Refresh</button>
+    </label>
+    <div id="chart" style="width:600px;height:300px" />
+  </div>
 </template>
